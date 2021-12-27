a31a2414e68fbbf0c44c4a1d58c7354410fa3d5a
Cartrell KIDD <cartrellk56@gmail.com>
2021-12-27 16:56:03 -0500

    Finalized Version

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const port = 8080;
+const port = process.env.port || 5000;
 const mainDir = path.join(__dirname, "/public");
 
 app.use(express.static('public'));
