799a85d209e0b9bacb9a14e48db1d7c1608033e7
Cartrell KIDD <cartrellk56@gmail.com>
2022-01-20 19:26:25 -0500

    converted routes to server.js

--- server.js
+++ server.js
@@ -1,21 +1,73 @@
-// Require Dependencies
 const express = require("express");
-const fs = require("fs");
-const path = require('path');
-
-// Initialize express app
 const app = express();
+const path = require("path");
+const fs = require("fs");
+const uuid = require("uuid");
+
 const PORT = process.env.PORT || 3000;
 
-// Setup data parsing
+
+
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname));
 
-//Require routes file
-require('./routes/routes')(app);
 
-// Setup listener
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+
+app.get("/api/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        res.json(notes);
+    })
+});
+
+
+app.post("/api/notes", function(req, res) {
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const newNote = req.body;
+        newNote.id = uuid.v4();
+        notes.push(newNote);
+
+        const createNote = JSON.stringify(notes);
+        fs.writeFile(path.join(__dirname, "./db/db.json"), createNote, (err) =>{
+            if (err) throw err;
+        });
+        res.json(newNote);
+    });
+});
+
+
+app.delete("/api/notes/:id", function(req, res) {
+    const noteID = req.params.id;
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const notesArray = notes.filter(item => {
+            return item.id !== noteID
+        });
+        fs.writeFile('./db/db.json', JSON.stringify(notesArray), (err, data) => {
+            console.log("Deletelos")
+            if (err) throw err; 
+            res.json(notesArray) 
+
+        });
+    });
+
+});
+
+
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-});  
+    console.log(`App listening to ${PORT}`);
+});
